1a3395035000124a0ce3e5d8da2e9e09f93495f7
Ryan485 <song976431@gmail.com>
2024-12-02 12:18:13 -0800

    Complete unit tests for test_boss_attributes

--- test_boss_attributes.py
+++ test_boss_attributes.py
@@ -6,4 +6,4 @@ class Test(TestCase):
     def test_boss_attributes(self):
         expected = {"Damage": 200, "HP": 2000}
         actual = game.boss_attributes()
-        self.assertEqual(expected, actual)/
+        self.assertEqual(expected, actual)
